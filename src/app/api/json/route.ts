f0494306c2d2db066543d0f8037299dd7a1125ac
devgoncalo <goncalopintoneto@gmail.com>
2024-06-12 16:08:05 +0200

    feat: ollama ai integration

--- src/app/api/json/route.ts
+++ src/app/api/json/route.ts
@@ -1,6 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
 import { ZodTypeAny, z } from "zod";
 
-import { NextRequest } from "next/server";
+import { openai } from "@/lib/openai";
+
+import { EXAMPLE_ANSWER, EXAMPLE_PROMPT } from "./example";
 
 const determineSchemaType = (schema: any): string => {
   if (!schema.hasOwnProperty("type")) {
@@ -34,9 +37,28 @@ const jsonSchemaToZod = (schema: any): ZodTypeAny => {
       }
       return z.object(shape);
     default:
-      throw new Error(`Unsupported schema type: ${type}!`);
+      throw new Error(`Unsupported schema type: ${type}`);
   }
 };
+
+type PromiseExecutor<T> = (
+  resolve: (value: T) => void,
+  reject: (reason?: any) => void
+) => void;
+
+class RetryablePromise<T> extends Promise<T> {
+  static async retry<T>(
+    retries: number,
+    executor: PromiseExecutor<T>
+  ): Promise<T> {
+    return new RetryablePromise<T>(executor).catch((error) => {
+      console.error(`Retrying due to error: ${error}`);
+      return retries > 0
+        ? RetryablePromise.retry(retries - 1, executor)
+        : RetryablePromise.reject(error);
+    });
+  }
+}
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
@@ -50,22 +72,49 @@ export const POST = async (req: NextRequest) => {
 
   const dynamicSchema = jsonSchemaToZod(format);
 
-  type PromiseExecutor<T> = (
-    resolve: (value: T) => void,
-    reject: (reason?: any) => void
-  ) => void;
+  const content = `DATA: \n"${data}"\n\n-----------\nExpected JSON format: ${JSON.stringify(
+    format,
+    null,
+    2
+  )}\n\n-----------\nValid JSON output in expected format:`;
 
-  class RetryablePromise<T> extends Promise<T> {
-    static async retry<T>(retries: number, executor: PromiseExecutor<T>): Promise<T> {
-      return new RetryablePromise(executor).catch((error) => {
-        console.error("Retrying due to error:", error);
+  const validationResult = await RetryablePromise.retry<string>(
+    3,
+    async (resolve, reject) => {
+      try {
+        const res = await openai.chat.completions.create({
+          model: "llama3",
+          messages: [
+            {
+              role: "assistant",
+              content:
+                "You are an AI that converts unstructured data into the attached JSON format. You respond with nothing but valid JSON based on the input data. Your output should DIRECTLY be valid JSON, nothing added before or after. You will begin right with the opening curly brace and end with the closing curly brace. Only if you absolutely cannot determine a field, use the value null.",
+            },
+            {
+              role: "user",
+              content: EXAMPLE_PROMPT,
+            },
+            {
+              role: "system",
+              content: EXAMPLE_ANSWER,
+            },
+            {
+              role: "user",
+              content,
+            },
+          ],
+        });
 
-        return retries > 0
-          ? RetryablePromise.retry(retries - 1, executor)
-          : RetryablePromise.reject(error);
-      });
+        const text = res.choices[0].message.content;
+
+        const validationResult = dynamicSchema.parse(JSON.parse(text || ""));
+
+        return resolve(validationResult);
+      } catch (err) {
+        reject(err);
+      }
     }
-  }
+  );
 
-  return new Response("OKAY");
+  return NextResponse.json(validationResult, { status: 200 });
 };
