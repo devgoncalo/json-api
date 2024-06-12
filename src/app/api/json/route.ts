e08c637323e94a370392449a5e73a2b016a021c3
devgoncalo <goncalopintoneto@gmail.com>
2024-06-12 15:54:03 +0200

    feat: schema creation for expected user format

--- src/app/api/json/route.ts
+++ src/app/api/json/route.ts
@@ -1,7 +1,54 @@
-import { NextRequest } from "next/server"
+import { ZodTypeAny, z } from "zod"
+
+import { NextRequest } from "next/server";
+
+const determineSchemaType = (schema: any): string => {
+  if (!schema.hasOwnProperty("type")) {
+    if (Array.isArray(schema)) {
+      return "array";
+    } else {
+      return typeof schema;
+    }
+  }
+  return schema.type;
+};
+
+const jsonSchemaToZod = (schema: any): ZodTypeAny => {
+  const type = determineSchemaType(schema);
+
+  switch (type) {
+    case "string":
+      return z.string().nullable();
+    case "number":
+      return z.number().nullable();
+    case "boolean":
+      return z.boolean().nullable();
+    case "array":
+      return z.array(jsonSchemaToZod(schema.items)).nullable();
+    case "object":
+      const shape: Record<string, ZodTypeAny> = {};
+      for (const key in schema) {
+        if (key !== "type") {
+          shape[key] = jsonSchemaToZod(schema[key]);
+        }
+      }
+      return z.object(shape);
+    default:
+      throw new Error(`Unsupported schema type: ${type}!`);
+  }
+};
 
 export const POST = async (req: NextRequest) => {
-  const body = await req.json()
+  const body = await req.json();
 
-  return new Response(JSON.stringify(body))
-}
+  const genericSchema = z.object({
+    data: z.string(),
+    format: z.object({}).passthrough(),
+  });
+
+  const { data, format } = genericSchema.parse(body);
+
+  const dynamicSchema = jsonSchemaToZod(format);
+
+  return new Response("OKAY");
+};
